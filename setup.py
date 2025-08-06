02c85f165603723de12eb6357bf8ff9055f01796
lzgirlcat <lz@girlc.at>
2025-08-06 18:07:32 +0200

    fix name_to_slug for "frankfurt/oder"

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def parse_requirements_file(path):
 
 setuptools.setup(
     name="koleo-cli",
-    version="0.2.137.23.2",
+    version="0.2.137.23.3",
     description="Koleo CLI",
     long_description=long_description(),
     long_description_content_type="text/markdown",
